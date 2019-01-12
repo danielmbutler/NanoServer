5e97b2f56510bb2ee8b9d0067abc2613c3158e71
danielmbutler <46110343+danielmbutler@users.noreply.github.com>
2019-01-12 21:48:30 +0000

    Add files via upload
    Deploy Nano Server with IIS, FailoverClustering and DNS 
    
    Set up quick IIS Site

---
+++ NanoServer Clean.ps1
@@ -0,0 +1,12 @@
+﻿####Build Nano Server##########
+New-NanoServerImage -MediaPath $path -Edition 'Datacenter' -DeploymentType Guest -TargetPath $logpath
+###################################
+
+
+#Connect to Nano Server and Deploy Site
+
+$ip = "192.168.0.125"  # replace with your Nano Server's IP address
+$s = New-PSSession -ComputerName $ip -Credential ~\Administrator
+Copy-Item -ToSession $s -Path $site -Recurse -Destination C:\Web2
+Copy-Item -FromSession $s -Path c:\web1\* -Destination C:\Nano
+New-IISSite -Name site -PhysicalPath c:\web2\Gym -BindingInformation "*:80:" -Force
